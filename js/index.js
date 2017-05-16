7c0aadac9d93f587aff306839fc62e9a293ae6e5
Collin Pham <collinpham2018@u.northwestern.edu>
2017-05-16 15:09:02 -0500

    Sprite verticle jumping only

--- js/index.js
+++ js/index.js
@@ -1,15 +1,76 @@
-window.onload = function() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
-    function preload () {
-        game.load.image('logo', './assets/phaser.png');
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('background', 'assets/background.png');
+
+}
+
+var player;
+var facing = 'right';
+var jumpTimer = 0;
+var cursors;
+var jumpButton;
+var bg;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+    game.physics.arcade.gravity.y = 300;
+
+    player = game.add.sprite(150, 320, 'dude');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 1000;
+    player.body.maxVelocity.y = 500;
+    player.body.setSize(20, 32, 5, 16);
+
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('turn', [4], 20, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+}
+
+function update() {
+    player.body.velocity.x = 0;
+
+    if (facing != 'idle')
+    {
+        player.animations.stop();
+
+        if (facing == 'left')
+        {
+            player.frame = 0;
+        }
+        else
+        {
+            player.frame = 5;
+        }
+
+        facing = 'idle';
+    }
+    
+    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    {
+        player.body.velocity.y = -500;
+        jumpTimer = game.time.now + 750;
     }
 
-    function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+}
 
-    }
+function render () {
 
-};
+    // game.debug.text(game.time.physicsElapsed, 32, 32);
+    // game.debug.body(player);
+    game.debug.bodyInfo(player, 16, 24);
 
+}
