fcd86285c74c0c96ef1fc9b88b128211fb4d548f
Collin Pham <collinpham2018@u.northwestern.edu>
2017-05-16 16:14:28 -0500

    collision events added

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload:
 function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.image('background', 'assets/background.png');
+    game.load.image('mushroom', 'assets/mushroom.png');
 }
 // define initial parameters
 var player;
+var obstacle;
 var facing = 'idle';
 var jumpTimer = 0;
 var runTimer = 0;
@@ -17,14 +19,14 @@ var bg;
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    // create the background
+    game.world.setBounds(0, 0, 1000, 600);
+    // Define background
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
 
     // define the y gravity
     game.physics.arcade.gravity.y = 300;
 
-    // create the sprite
+    // Define player
     player = game.add.sprite(150, 320, 'dude');
     player.frame = 5;
     game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -33,6 +35,19 @@ function create() {
     player.body.gravity.y = 1000;
     player.body.maxVelocity.y = 500;
     player.body.setSize(20, 32, 5, 16);
+    player.body.immovable = true;
+
+    // Define obstacle
+    
+
+    obstacle = game.add.sprite(900, 600, 'mushroom');
+    obstacle.name = 'mushroom';
+    
+    game.physics.enable(obstacle, Phaser.Physics.ARCADE);
+    
+    obstacle.body.velocity.x = -100
+    obstacle.body.collideWorldBounds = true;
+
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -41,6 +56,7 @@ function create() {
 
 function update() {
     player.body.velocity.x = 0;
+    game.physics.arcade.collide(player, obstacle, collisionHandler, null, this);
 
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
@@ -53,6 +69,19 @@ function update() {
         runTimer = game.time.now + 250
         player.frame = (player.frame % 4 + 5)
     }
+    if (obstacle.x == 0) {
+        console.log('destroy')
+    }
+
+}
+function collisionHandler (obj1, obj2) {
+
+    game.stage.backgroundColor = '#992d2d';
+    console.log('BOOM')
+
+}
+
+function renderObstacle (id) {
 
 }
 
