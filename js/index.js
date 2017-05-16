7bba7f1d55cd181d09c31b9aa9ad3ca4628c576b
Collin Pham <collinpham2018@u.northwestern.edu>
2017-05-16 15:24:08 -0500

    running sprite

--- js/index.js
+++ js/index.js
@@ -1,39 +1,38 @@
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
+// Load the assets
 function preload() {
-
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     game.load.image('background', 'assets/background.png');
-
 }
-
+// define initial parameters
 var player;
-var facing = 'right';
+var facing = 'idle';
 var jumpTimer = 0;
+var runTimer = 0;
 var cursors;
 var jumpButton;
 var bg;
 
 function create() {
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    // create the background
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
 
+    // define the y gravity
     game.physics.arcade.gravity.y = 300;
 
+    // create the sprite
     player = game.add.sprite(150, 320, 'dude');
+    player.frame = 5;
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.collideWorldBounds = true;
     player.body.gravity.y = 1000;
     player.body.maxVelocity.y = 500;
     player.body.setSize(20, 32, 5, 16);
-
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -43,26 +42,16 @@ function create() {
 function update() {
     player.body.velocity.x = 0;
 
-    if (facing != 'idle')
-    {
-        player.animations.stop();
-
-        if (facing == 'left')
-        {
-            player.frame = 0;
-        }
-        else
-        {
-            player.frame = 5;
-        }
-
-        facing = 'idle';
-    }
-    
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
+        player.frame = 6
         player.body.velocity.y = -500;
         jumpTimer = game.time.now + 750;
+    }
+    if (player.body.onFloor() && game.time.now > runTimer) 
+    {
+        runTimer = game.time.now + 250
+        player.frame = (player.frame % 4 + 5)
     }
 
 }
